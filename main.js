212b73fd27bcfb0e73e72cb3944a7d8bed2b917b
Tanisha-2010 <104196498+Tanisha-2010@users.noreply.github.com>
2022-07-25 15:18:49 +0530

    html button modified

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg", "https://i.postimg.cc/qqyYvVbq/grandpa.jpg", "https://i.postimg.cc/wjMnFtMX/father.jpg", "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
-var names = ["Family Book", "Ranbir Singh", "Diljeet Singh", "Rocky Singh", "Alia Singh", "Soni Singh"];
+var names = ["Fmaily Book", "Ranbir Singh", "Diljeet Singh", "Rocky Singh", "Alia Singh", "Soni Singh"];
 var i = 0;
 
 function update() {
@@ -10,9 +10,9 @@ function update() {
     }
 
     //Debug the code to store list of images in updatedImage. Use images[i]
-    var updatedImage = [i];
+    var updatedImage = images[i];
     //Debug the code to store list of names in updatedName. Use names[i]
-    var updatedName = [i];
+    var updatedName = names[i];
 
     document.getElementById("family_member_image").src = updatedImage;
     document.getElementById("family_member_name").innerHTML = updatedName;
